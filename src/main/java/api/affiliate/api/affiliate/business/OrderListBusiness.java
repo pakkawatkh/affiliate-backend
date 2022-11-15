d0aa0ad86cd6d5a0327dddcddf9dd5907cb98544
orawan225 <ot.orawan2509@gmail.com>
2022-11-15 22:14:33 +0700

    test1

--- src/main/java/api/affiliate/api/affiliate/business/OrderListBusiness.java
+++ src/main/java/api/affiliate/api/affiliate/business/OrderListBusiness.java
@@ -101,11 +101,7 @@ public class OrderListBusiness {
         checkRoleIsStore(user);
         StoreTable store = storeService.findByUserId2(user);
         Object orderList = orderService.getTotalPriceByOrderStatusSuccess(store.getStoreId());
-<<<<<<< HEAD
-        return new Response().ok("", "TotalMoney", orderList);
-=======
         return new Response().ok("", "total_price", orderList);
->>>>>>> 329a93fee1412468328c7569b26132c702480206
     }
 
 
